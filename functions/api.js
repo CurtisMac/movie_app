d91830a6b603640860fd0b714e049bbc7b214543
Curtis M <34104460+CurtisMac@users.noreply.github.com>
2018-02-09 21:41:39 -0800

    basic functionality

--- functions/api.js
+++ functions/api.js
@@ -2,24 +2,35 @@ const request = require('request')
 const url = 'https://api.themoviedb.org/3/discover/movie?api_key=1eb7ae5fe70ccaa91530537bdf3dc0ea&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1'
 
 module.exports = {
-    getMovies: function (callback) {
+    getMovies: function (renderMovies) {
         request(url, (err, res, body) => {
             if (!err) {
                 let data = JSON.parse(body)
                 let moviesObj = data.results
-                callback(moviesObj)
+                renderMovies(moviesObj)
             } else {
                 console.log(`there was an error ${err}`)
             }
         })
     },
 
-    getMovie: function (movieId, callback) {
+    getMovie: function (movieId, renderMovie) {
         request(`https://api.themoviedb.org/3/movie/${movieId}?api_key=1eb7ae5fe70ccaa91530537bdf3dc0ea`, (err, res, body) => {
             if (!err) {
-                let data = JSON.parse(body)
-                let movie = data
-                callback(movie)
+                let movie = JSON.parse(body)
+                renderMovie(movie)
+            } else {
+                console.log(`there was an error ${err}`)
+            }
+        })
+    },
+
+    searchMovies: function (searchTerm, renderResults) {
+        request(`https://api.themoviedb.org/3/search/movie?api_key=1eb7ae5fe70ccaa91530537bdf3dc0ea&query=${searchTerm}`, (err, res, body) => {
+            if (!err) {
+                let data =  JSON.parse(body)
+                let results = data.results
+                renderResults(results)
             } else {
                 console.log(`there was an error ${err}`)
             }
--- routes/searchRouter.js
+++ routes/searchRouter.js
@@ -1,13 +1,14 @@
 const express = require('express')
-const movies = require('../functions/api')
+const api = require('../functions/api')
 const searchRouter = express.Router()
 
 searchRouter.get('/', (req, res) => {
     let { searchTerm } = req.query
-    let movies = 0
-    console.log(searchTerm)
-    res.send(searchTerm)
-    // res.render('index', {movies})
+    api.searchMovies(searchTerm, (results) => {
+        res.render('index', {
+            movies: results
+        })
+    })
 })
 
 module.exports = searchRouter
